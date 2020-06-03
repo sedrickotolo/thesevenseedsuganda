a7c632eb52f264433efe2922bb34987d8825aca7
sedrickotolo <41705210+sedrickotolo@users.noreply.github.com>
2020-06-03 03:27:51 +0300

    New Upates

--- contact-form.php
+++ contact-form.php
@@ -1,4 +1,25 @@
 <?php
+
+if(isset($_POST['submit']))
+{
+      $name = $_POST['name'];
+      $subject = $_POST['subject'];
+      $mailFrom = $_POST['email'];
+      $message = $_POST['message'];
+
+      $mailTo = "sedricksedu2@gmail.com";
+      $headers = "From:" $mailFrom;
+      $txt = "You have received an email from ".$name.".\n\n".$message;
+       
+
+      mail($mailTo,$subject,$txt,$headers);
+
+      header("Location: index.php?mailsend");
+
+
+}
+
+
       $message = $_POST['message'];
       $name = $_POST['name'];
       $visitor_email = $_POST['email'];
